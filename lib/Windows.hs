c34791cc73e52e6df5a220d7095c4ee577184920
davho <david.and.hoffmann+github@gmail.com>
2014-07-18 16:01:05 +0200

    changed window rules a bit

--- lib/Windows.hs
+++ lib/Windows.hs
@@ -10,21 +10,22 @@ import XMonad.Hooks.ManageHelpers
 -- window rules
 manageHook' = (composeAll . concat $
     [ [anyQuery x --> doIgnore               | x <- ignore]
-    , [anyQuery x --> doShift "terminal"     | x <- terminal]
     , [anyQuery x --> doShift "0:email"      | x <- email]
-    , [anyQuery x --> doShift "1:web"        | x <- web]
     , [anyQuery x --> doShift "2"            | x <- dev]
     , [anyQuery x --> doShift "3"            | x <- java]
     -- , [anyQuery x --> doShift "vnc"       | x <- vnc]
     , [anyQuery x --> doShift "7:media"      | x <- media]
     , [anyQuery x --> doShift "10:trash"     | x <- trash]
-    , [anyQuery x --> doCenterFloat          | x <- center]
+    -- , [anyQuery x --> doCenterFloat          | x <- center]
     , [anyQuery x --> doFloat                | x <- float' ++ java]
 
-    , [x --> doFloat          | x <- splash ++ float]
-    , [x --> doShift "9:top"  | x <- top]
-    , [x --> doShift "8:chat" | x <- chat]
+    , [x --> doFloat            | x <- splash ++ float]
+    , [x --> doShift "1:web"    | x <- web]
+    , [x --> doShift "8:chat"   | x <- chat]
+    , [x --> doShift "9:top"    | x <- top]
+    , [x --> doShift "terminal" | x <- terminal]
 
+    , [isFullscreen --> doFullFloat]
     , [manageDocks]
     ])
     <+> manageSpawn
@@ -34,27 +35,33 @@ manageHook' = (composeAll . concat $
     where
         email    = ["mailClient", "Mail", "Thunderbird", "mutt"]
         ignore   = ["desktop_window", "desktop", "notify-osd", "trayer", "stalonetray"]
-        web      = ["Firefox", "Chromium", "Google-chrome", "Chromium-browser"]
+        -- web'      = ["Firefox", "Chromium", "Google-chrome", "Chromium-browser"]
         dev      = ["Eclipse"]
         float'   = ["File Transfers", "java", "Steam", "dota_linux", "mandelbrot"]
         center   = ["MPlayer", "Plugin-container"]
-        media    = ["youtube-viewer", "musicPlayer", "spotify", "plugin-container"]
+        media    = ["youtube-viewer", "musicPlayer", "spotify"] --, "plugin-container"]
         vnc      = ["vncviewer", "vinagre"]
         java     = ["Intelligent SpeedMeter", "sun-awt-X11-XFramePeer", "Main", "MeinProgramm",
                     "openDLX-main-OpenDLXSimulatorMain", "u11-gui-Main", "java"]
         trash    = ["offlineimap", "vino"]
-        terminal = ["terminal"]
 
 
         float  = [title =? "Firefox Preferences"]
         splash = [title =? "Wireshark"]
-        top    =
+        terminal = [ className =? "terminal"]
+        top =
             [ title =? "htop"
-            , title =? "powertop"]
-        chat   =
+            , title =? "powertop"
+            ]
+        chat =
             [ title     =? "ircClient"
             , className =? "Skype"
             , className =? "Pidgin"
-            , className =? "Gajim"]
+            , className =? "Gajim"
+            ]
+        web =
+            [ className =? "Firefox"
+            , className =? "Chromium"
+            ]
 
         anyQuery x = fmap or . mapM (=? x) $ [resource, className, title]
