735f8202d7a275bebcf89deb5fd5c37ac84772f4
davho <david.and.hoffmann+github@gmail.com>
2015-05-01 19:31:35 +0200

    proper (manual) merge

--- lib/StartUp.hs
+++ lib/StartUp.hs
@@ -34,8 +34,7 @@ startupHook' host = case host of
 
         -- screen configuration
         xfork $ screenSetup host
-        -- no jabber, but telegram
-        spawnOnceSleep 10 telegram
+        spawnOnceSleep 10 telegramClient
 
         defaultStartupHook
 
