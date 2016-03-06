41fb5a33abe6ff840d71aa5d2efde4a61827dbac
David Hoffmann <david.and.hoffmann+github@gmail.com>
2016-03-06 18:22:06 +0100

    cleanup border colors

--- lib/BorderColors.hs
+++ lib/BorderColors.hs
@@ -1,8 +1,11 @@
 module BorderColors where
 
--- border colors for unfocused and focused windows.
-normalBorderColor'  = "#000000"
--- focusedBorderColor' = "#dddddd"
+-- border colors for unfocused windows
+normalBorderColor' :: String
+normalBorderColor' = "#000000"
+
+-- border colors for focused windows
+--   - #729fcf is tango blue
+--   - #555753 is tango lightblack
+focusedBorderColor' :: String
 focusedBorderColor' = "#555753"
--- #729fcf is tango blue
--- #555753 is tango lightblack
